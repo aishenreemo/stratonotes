695741df83ada55fb55a4a680bfd95d5d8662c47
Cryonixx <cryon.ixx911@gmail.com>
2025-05-03 14:36:35 +0800

    chore: made sidebar resizeable (has a bit of jank)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,22 @@
+import { Resizable } from 're-resizable';
+
 function Sidebar({ isOnLeft, opened }: { isOnLeft: boolean; opened: boolean }) {
     return (
-        <div
-            style={{
-                gridColumn: isOnLeft ? "2 / 3" : "4 / 5",
+        <Resizable
+            className="box-border border border-black transition-all 0.05s overflow-x-hidden"
+            minWidth={opened ? 192 : 0}
+            maxWidth={384}
+            enable={{
+                left: !isOnLeft,
+                right: isOnLeft
             }}
-            className={[
-                "flex flex-col transition-all",
-                "duration-300 overflow-x-hidden",
-                opened ? "w-[12rem] border" : "w-0",
-            ].join(" ")}
-        ></div>
+            style={{
+                gridColumn: isOnLeft ? "2 / 3" : "4 / 5"
+            }}
+
+            size={{width: opened ?  "16rem" : 0}}>
+        </Resizable>
+
     );
 }
 
