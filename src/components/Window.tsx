42a42c665d4a4c644b5d2b2825ddf8c94070ec73
Aishen Reemo <aish3n@pm.me>
2025-05-16 15:46:58 +0800

    feat: add Window

---
+++ src/components/Window.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+function Window({ width, height, children }: {
+    width: number,
+    height: number,
+    children?: ReactNode
+}) {
+    return (
+        <div className={`absolute w-[${width}%] h-[${height}%] border rounded-2xl`}>
+            {children}
+        </div>
+    );
+}
+
+export default Window;
